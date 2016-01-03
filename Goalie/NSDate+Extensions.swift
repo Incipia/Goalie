84083e2ccee015e91fd0b1873329449d0121ea64
gregoryklein <gregoryhklein@gmail.com>
2016-01-02 20:46:16 -0500

    Added some useful extension methods for NSDate

--- Goalie/NSDate+Extensions.swift
+++ Goalie/NSDate+Extensions.swift
@@ -7,3 +7,79 @@
 //
 
 import Foundation
+
+struct DateFormatter
+{
+   static private var sharedInstance = DateFormatter()
+   
+   static var formatter: NSDateFormatter {
+      return sharedInstance.formatter
+   }
+   
+   private lazy var formatter: NSDateFormatter = {
+      let df = NSDateFormatter()
+      return df
+   }()
+}
+
+extension NSDate
+{
+   var hour: Int {
+   return NSCalendar.autoupdatingCurrentCalendar().components((.Hour), fromDate: self).hour
+   }
+   
+   var minute: Int {
+      return NSCalendar.autoupdatingCurrentCalendar().components((.Minute), fromDate: self).minute
+   }
+   
+   var day: Int {
+      return NSCalendar.autoupdatingCurrentCalendar().components((.Day), fromDate: self).day
+   }
+   
+   var month: Int {
+      return NSCalendar.autoupdatingCurrentCalendar().components((.Month), fromDate: self).month
+   }
+   
+   func logHourAndMinute()
+   {
+      print("hour: \(self.hour) minute: \(self.minute)")
+   }
+   
+   func prettyDateString() -> String
+   {
+      let formatter = NSDateFormatter()
+      formatter.dateStyle = NSDateFormatterStyle.LongStyle
+      formatter.timeStyle = .MediumStyle
+      
+      return formatter.stringFromDate(self)
+   }
+   
+   func dayString() -> String
+   {
+      let formatter = DateFormatter.formatter
+      formatter.dateFormat = "EEEE"
+      return formatter.stringFromDate(self)
+   }
+}
+
+func <(l: NSDate, r: NSDate) -> Bool {
+   return l.compare(r) == .OrderedAscending
+}
+
+func >(l: NSDate, r: NSDate) -> Bool {
+   return l.compare(r) == .OrderedDescending
+}
+
+func ==(l: NSDate, r: NSDate) -> Bool {
+   return l.compare(r) == .OrderedSame
+}
+
+func <=(l: NSDate, r: NSDate) -> Bool {
+   let result = l.compare(r)
+   return result == .OrderedAscending || result == .OrderedSame
+}
+
+func >=(l: NSDate, r: NSDate) -> Bool {
+   let result = l.compare(r)
+   return result == .OrderedDescending || result == .OrderedSame
+}
