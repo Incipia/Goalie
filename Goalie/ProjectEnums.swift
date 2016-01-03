f149ce7b4525b6e7cd0be73f8a719fdc4d1f3b5b
gregoryklein <gregoryhklein@gmail.com>
2016-01-02 20:47:40 -0500

    Removed unused enums

--- Goalie/ProjectEnums.swift
+++ Goalie/ProjectEnums.swift
@@ -8,68 +8,7 @@
 
 import Foundation
 
-extension NSDate {
-   var month: Int {
-      return NSCalendar.currentCalendar().components((.Month), fromDate: self).month
-   }
-}
-
-public enum ActiveState: Int {
-   case Today, Tomorrow, Idle
-}
-
-public enum TaskPriority: Int {
-   case ASAP, Soon, Later, Ages
-}
-
-public enum Month: Int {
-   case Jan, Feb, Mar, Apr, May, Jun, Jul, Aug, Sep, Oct, Nov, Dec
-   
-   init(date: NSDate)
-   {
-      var rawValue = date.month - 1
-      if rawValue < 0 || rawValue >= 12 {
-         rawValue = 0
-      }
-      self.init(rawValue: rawValue)!
-   }
-}
-
-extension Month
+public enum TaskPriority: Int
 {
-   var text: String {
-      switch self
-      {
-      case .Jan: return "JAN"
-      case .Feb: return "FEB"
-      case .Mar: return "MAR"
-      case .Apr: return "APR"
-      case .May: return "MAY"
-      case .Jun: return "JUN"
-      case .Jul: return "JUL"
-      case .Aug: return "AUG"
-      case .Sep: return "SEP"
-      case .Oct: return "OCT"
-      case .Nov: return "NOV"
-      case .Dec: return "DEC"
-      }
-   }
-   
-   var fullName: String {
-      switch self
-      {
-      case .Jan: return "January"
-      case .Feb: return "February"
-      case .Mar: return "March"
-      case .Apr: return "April"
-      case .May: return "May"
-      case .Jun: return "June"
-      case .Jul: return "July"
-      case .Aug: return "August"
-      case .Sep: return "September"
-      case .Oct: return "October"
-      case .Nov: return "November"
-      case .Dec: return "December"
-      }
-   }
+   case ASAP, Soon, Later, Ages
 }
