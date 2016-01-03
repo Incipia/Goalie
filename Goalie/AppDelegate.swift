aaafd7a0f637b86a95f99dc58c225888188746e5
gregoryklein <gregoryhklein@gmail.com>
2016-01-02 20:49:01 -0500

    A little refactoring and added started writing code to test out automatic priority incrementing

--- Goalie/AppDelegate.swift
+++ Goalie/AppDelegate.swift
@@ -15,21 +15,69 @@ import Crashlytics
 class AppDelegate: UIResponder, UIApplicationDelegate
 {
    var window: UIWindow?
-   var moc: NSManagedObjectContext!
+   var moc: NSManagedObjectContext! {
+      didSet {
+         _taskPriorityUpdater = TaskPriorityUpdater(managedObjectContext: moc)
+      }
+   }
+   
+   private var _taskPriorityUpdater: TaskPriorityUpdater!
+   private var _minuteChangedNotificationTimer: NSTimer?
+   
    private var _mainTasksViewController: MainTasksViewController!
 
    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
    {
       Fabric.with([Crashlytics.self])
-
-      _mainTasksViewController = UIStoryboard.mainTasksViewController()
-      _mainTasksViewController.moc = createGoalieMainContext()
       
+      moc = createGoalieMainContext()
+      _setupMainTasksViewControllerWithMOC(moc)
+      _setupMainWindowWithViewController(_mainTasksViewController)
+      
+      return true
+   }
+   
+   func applicationDidBecomeActive(application: UIApplication) {
+      _startTimerForMinuteChangedNotification()
+   }
+   
+   func applicationWillResignActive(application: UIApplication)
+   {
+      _minuteChangedNotificationTimer?.invalidate()
+      _minuteChangedNotificationTimer = nil
+   }
+   
+   private func _setupMainTasksViewControllerWithMOC(moc: NSManagedObjectContext)
+   {
+      _mainTasksViewController = UIStoryboard.mainTasksViewController(moc)
+   }
+   
+   private func _setupMainWindowWithViewController(controller: UIViewController)
+   {
       window = UIWindow(frame: UIScreen.mainScreen().bounds)
       window?.rootViewController = _mainTasksViewController
       window?.makeKeyAndVisible()
-      
-      return true
+   }
+
+   private func _startTimerForMinuteChangedNotification()
+   {
+      if _minuteChangedNotificationTimer == nil
+      {
+//         let calendar = NSCalendar.autoupdatingCurrentCalendar()
+//         let components = calendar.components(.Second, fromDate: NSDate())
+//         let currentSecond = components.second
+         
+//         let fireDate = NSDate().dateByAddingTimeInterval(NSTimeInterval(60 - currentSecond + 1))
+         let fireDate = NSDate().dateByAddingTimeInterval(NSTimeInterval(3))
+         _minuteChangedNotificationTimer = NSTimer(fireDate: fireDate, interval: 1, target: self, selector: Selector("_minuteChanged:"), userInfo: nil, repeats: true)
+         
+         NSRunLoop.mainRunLoop().addTimer(_minuteChangedNotificationTimer!, forMode: NSDefaultRunLoopMode)
+      }
+   }
+   
+   internal func _minuteChanged(timer: NSTimer)
+   {
+      _taskPriorityUpdater.updateTaskPriorities()
    }
 }
 
