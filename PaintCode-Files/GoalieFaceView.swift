b777bd0bc56c1883b967255b98067af614419ea8
gregoryklein <gregoryhklein@gmail.com>
2016-01-05 23:22:39 -0500

    Added new files for drawing goalies face

---
+++ PaintCode-Files/GoalieFaceView.swift
@@ -0,0 +1,21 @@
+//
+//  GoalieFaceView.swift
+//  Goalie
+//
+//  Created by Gregory Klein on 1/5/16.
+//  Copyright © 2016 Incipia. All rights reserved.
+//
+
+import UIKit
+
+class GoalieFaceView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
