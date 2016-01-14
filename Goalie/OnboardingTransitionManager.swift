3190fa43d079d77bc51b3ef4b95f4f3591fa6984
gregoryklein <gregoryhklein@gmail.com>
2016-01-13 21:32:51 -0500

    Adjusted volume

---
+++ Goalie/OnboardingTransitionManager.swift
@@ -0,0 +1,13 @@
+//
+//  OnboardingTransitionManager.swift
+//  Goalie
+//
+//  Created by Gregory Klein on 1/13/16.
+//  Copyright © 2016 Incipia. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingTransitionManager: NSObject {
+
+}
