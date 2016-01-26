a6a18aecf64879210d703224c9dd578de87f77d7
gregoryhklein@gmail.com <gregoryhklein@gmail.com>
2016-01-26 14:58:24 -0500

    Ripped out Fabric/Crashlytics

--- Goalie/AppDelegate.swift
+++ Goalie/AppDelegate.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 import CoreData
-import Fabric
-import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -32,8 +30,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate
    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
    {
       BuddyBuildSDK.setup()
-      
-      Fabric.with([Crashlytics.self])
       
       _moc = createGoalieMainContext()
       _setupMainTasksViewControllerWithMOC(_moc)
