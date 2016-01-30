e75ca0130f26b61e1058efc907f0f6e835c18e77
gregoryhklein@gmail.com <gregoryhklein@gmail.com>
2016-01-30 00:32:52 -0500

    Ripped out fabric and put buddybuild back in

--- Goalie/AppDelegate.swift
+++ Goalie/AppDelegate.swift
@@ -8,8 +8,6 @@
 
 import UIKit
 import CoreData
-import Fabric
-import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate
@@ -31,7 +29,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate
 
    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
    {
-      Fabric.with([Crashlytics.self])
+      BuddyBuildSDK.setup()
+      BuddyBuildSDK.setScreenshotAllowedCallback { () -> Bool in
+         return false
+      }
       
       _moc = createGoalieMainContext()
       _setupMainTasksViewControllerWithMOC(_moc)
